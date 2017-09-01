49225ea3423318b2bc91ea4ee9511024624fe576
zxyxx <379951650@qq.com>
2017-09-01 22:10:56 +0800

    add jquery

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Web security</title>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="js/jquery-3.2.1.min.js"></script>
     <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
     <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" crossorigin="anonymous">
     <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
