2abdde202b34f48d2a8d0237b609e0edfa7eadff
zxyxx <379951650@qq.com>
2017-09-03 21:34:23 +0800

    fix bug

--- js/load_category4menu.js
+++ js/load_category4menu.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     $.getJSON("index.php?/admin/show_category", function(result){
         $.each(result, function(num, value){
             var category = value.category;
-            var link = $('<a></a>').text(category).attr('href', 'http://www.au1ge.xyz/websec/index.php?/articles/search/'+category);
+            var link = $('<a></a>').text(category).attr('href', 'index.php?/articles/search/'+category);
             var opt = $('<li></li>').append(link);
             $("ul#category").append(opt);
         });
