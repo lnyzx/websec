06e089d0f708ed67b238b8dbe8cbef4571de5189
zxyxx <379951650@qq.com>
2018-03-17 12:34:15 +0800

    [+] change rss feed

--- application/controllers/Rss.php
+++ application/controllers/Rss.php
@@ -15,16 +15,19 @@ class Rss extends CI_Controller{
     public function index(){
         header("Content-type: application/rss+xml");
         $query  = $this -> article_model -> all_articles_rss();
+        $this_time = date("Y-m-d h:i:sa");
 
         echo <<<rss_start
-<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-<channel>
-<title>Lnyas`s WebSec</title>  
-<link>http://182.254.247.127/websec/</link>  
-<description>Focus on Web Security</description>  
-<language>en-us</language>
-<atom:link href="http://182.254.247.127/websec/index.php?/rss" rel="self" type="application/rss+xml" />
+<feed xmlns="http://www.w3.org/2005/Atom">
+  <title type="text">Lnyas`s WebSec</title>
+  <id>http://182.254.247.127/websec/index.php?/rss</id>
+  <updated>{$this_time}</updated>
+  <link href="http://182.254.247.127/websec/" />
+  <link href="http://182.254.247.127/websec/index.php?/rss" rel="self" />
+  <author>
+    <name>Unknown author</name>
+  </author>
+  <generator>Lnyas</generator>
 rss_start;
         foreach($query as $row){
             $title = htmlspecialchars($row->title);
@@ -34,19 +37,18 @@ rss_start;
             $time = date("D, d M y H:i:s O", $time);
             echo <<<item
     
-    <item>
-      <title>{$title}</title>
-      <description>{$desc}</description>
-      <link>{$link}</link>
-      <guid>{$link}</guid>
-      <pubDate>{$time}</pubDate>
-    </item>  
+  <entry xml:base="http://182.254.247.127/websec/index.php?/rss">
+    <title type="text">{$title}</title>
+    <id>{$link}</id>
+    <updated>{$time}</updated>
+    <link href="{$link}" />
+    <content type="html">{$desc}</content>
+  </entry>
 item;
         }
         echo <<<rss_end
         
-  </channel>
-</rss>  
+</feed>
 rss_end;
 
     }
